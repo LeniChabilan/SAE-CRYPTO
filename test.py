9ca6d906bf3ec4269b5e5a5fc2975339b36d2d19
Debray Antoine <o22202147@info23-21.iut45.univ-orleans.fr>
2023-12-19 09:43:05 +0100

    ajout des fonctions dechiffrement des Images

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
 from DES import *
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+import os
 import time
+from PIL import Image
+from binascii import unhexlify
+from scapy.all import *
+
 def chiffrage(message, cle):
     message_chiffre = ""
     for lettre in message:
         message_chiffre += chr(encrypt(cle, ord(lettre)))
     return message_chiffre
+
 def dechiffrage(message_chiffre, cle):
     message_clair = ""
     for lettre in message_chiffre:
@@ -59,8 +69,8 @@ message_clair = "hello"
 cle1 = 0000000000
 cle2 = 1111
 
-message_chiffre = chifrementSDES(message_clair, cle1, cle2)
-print(message_chiffre)
+# message_chiffre = chifrementSDES(message_clair, cle1, cle2)
+# print(message_chiffre)
 # time1=time.time()
 # cle_trouvee2 , cle_trouve = casse_brutal(message_clair, message_chiffre)
 # time2=time.time()
@@ -83,8 +93,36 @@ def cassage_astucieux(message_clair, message_chiffre):
             return liste[dechiffrage(message_chiffre, int(key_binary2))],key_binary2
         
     print("Clé inconnue")
-time1=time.time()
-cle_trouvee4 , cle_trouve3 = cassage_astucieux(message_clair, message_chiffre)
-time2=time.time()
-print(time2-time1)
-print("Clé trouvée :", cle_trouvee4 , cle_trouve3)
+
+# time1=time.time()
+# cle_trouvee4 , cle_trouve3 = cassage_astucieux(message_clair, message_chiffre)
+# time2=time.time()
+# print(time2-time1)
+# print("Clé trouvée :", cle_trouvee4 , cle_trouve3)
+
+
+
+def chiffrement_aes(key, texte_clair):
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+    chiffreur = cipher.encryptor()
+    # Ajout de remplissage (padding) pour garantir que la longueur est un multiple de la taille du bloc
+    padder = padding.PKCS7(128).padder()
+    texte_clair_padded = padder.update(texte_clair) + padder.finalize()
+    texte_chiffre = chiffreur.update(texte_clair_padded) + chiffreur.finalize()
+    return texte_chiffre
+
+def dechiffrement_aes(key, texte_chiffre):
+    cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
+    dechiffreur = cipher.decryptor()
+    texte_dechiffre = dechiffreur.update(texte_chiffre) + dechiffreur.finalize()
+    # Retirer le remplissage (padding)
+    unpadder = padding.PKCS7(128).unpadder()
+    texte_clair = unpadder.update(texte_dechiffre) + unpadder.finalize()
+    return texte_clair
+
+texte_clair_connu = b'bonjour a tous'  
+cle_aes = os.urandom(32)
+texte_chiffre = chiffrement_aes(cle_aes, texte_clair_connu)
+cle_devinee = dechiffrement_aes(cle_aes,texte_chiffre)
+# print(texte_chiffre)
+# print(cle_devinee)
