8c21836f566b7729d627af54242f7c307a39e69d
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2023-12-21 16:05:59 +0100

    ajout de la partie 3 et refactoring des fichier pour ergonomie

--- Image.py
+++ Image.py
@@ -13,14 +13,47 @@ def analyse_image(chemin):
     return res
 
 
-resultat=analyse_image("rossignol2.bmp")
+resultat=analyse_image("Image/rossignol2.bmp")
 mes=resultat[:64]
-print(mes)
 
-def binaire_to_hexa(binaire):
-    decimal=int(binaire,2)
-    hexa=hex(decimal)[2:]
-    return hexa.upper()
-hexa=binaire_to_hexa(mes)
-print(hexa)
-    
+
+
+from scapy.all import UDP  # Ajoutez cette ligne pour importer explicitement UDP
+
+from scapy.all import *
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+# Function to decrypt AES-256 CBC encrypted message with PKCS7 padding
+def decrypt_aes_cbc(ciphertext, key, iv):
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    unpadder = padding.PKCS7(128).unpadder()
+
+    decrypted_data = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+
+    return unpadded_data
+
+# Load the network capture file
+capture = rdpcap('./analyse_trace/trace_sae.cap') 
+# Replicate the key 4 times to obtain the 256-bit key
+full_key = bytes([int(mes[i:i+8], 2) for i in range(0, len(mes), 8)]) * 4
+
+# Define the protocol and port
+protocol = 'plutotBonneConfidentialite'
+port = 9999
+
+# Iterate through the packets
+for packet in capture:
+    if UDP in packet and packet[UDP].dport == port:
+        # Extract IV and encrypted message
+        iv = packet[Raw].load[:16]
+        ciphertext = packet[Raw].load[16:]
+
+        # Decrypt the message
+        decrypted_message = decrypt_aes_cbc(ciphertext, full_key, iv)
+
+        # Print or process the decrypted message
+        print(decrypted_message.decode('utf-8', errors='ignore'))
